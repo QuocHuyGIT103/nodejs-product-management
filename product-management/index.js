bcb8e03096c3c40159a81f84da3d5a342002121d
QuocHuyGIT103 <quochuyab2003@gmail.com>
2025-02-13 16:02:39 +0700

    install mongooso and insert model product

--- product-management/index.js
+++ product-management/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 require('dotenv').config()
+
 const app = express()
 const port = process.env.PORT
 
+const database = require('./config/database.js')
+
+database.connect();
 
 const route = require('./routes/client/index.route');
 
