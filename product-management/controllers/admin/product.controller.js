b36cf02cb3c4ad9d17a6b5e0284fe220d916e636
QuocHuyGIT103 <quochuyab2003@gmail.com>
2025-02-20 23:03:14 +0700

    xóa tạm thời item

--- product-management/controllers/admin/product.controller.js
+++ product-management/controllers/admin/product.controller.js
@@ -92,7 +92,9 @@ module.exports.changeMulti = async (req, res) => {
 module.exports.deleteItem = async (req, res) => {
   const id = req.params.id;
 
-  await Product.deleteOne({ _id: id });
+  // await Product.deleteOne({ _id: id }); xóa vĩnh viễn
+
+  await Product.updateOne({ _id: id }, { deleted: true }); // xóa tạm thời
 
   res.redirect("back");
 };
