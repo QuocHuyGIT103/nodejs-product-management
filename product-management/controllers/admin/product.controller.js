ac560cb3e36324d3175448628d3f65fbe4f9a3c7
QuocHuyGIT103 <quochuyab2003@gmail.com>
2025-02-22 20:47:05 +0700

    tính năng thêm mới sản phẩm

--- product-management/controllers/admin/product.controller.js
+++ product-management/controllers/admin/product.controller.js
@@ -2,6 +2,8 @@
 const filterStatus = require("../../helpers/filterStatus");
 const Product = require("../../models/product.model");
 
+const systemConfig = require("../../config/system");
+
 const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search");
 const paginationHelper = require("../../helpers/pagination");
@@ -129,4 +131,30 @@ module.exports.deleteItem = async (req, res) => {
   ); // xóa tạm thời
   req.flash("success", `Đã xóa 1 sản phẩm thành công!`);
   res.redirect("back");
+};
+
+//[GET] /admin/products/create
+module.exports.create = async (req, res) => {
+  res.render("admin/pages/products/create", {
+    pageTitle: "Tạo mới SP",
+  });
+};
+
+//[POST] /admin/products/create
+module.exports.createPost = async (req, res) => {
+  req.body.price = parseInt(req.body.price);
+  req.body.discountPercentage = parseInt(req.body.discountPercentage);
+  req.body.stock = parseInt(req.body.stock);
+
+  if (req.body.position == "") {
+    const countProducts = await Product.countDocuments();
+    req.body.position = countProducts + 1;
+  } else {
+    req.body.position = parseInt(req.body.position);
+  }
+
+  const product = new Product(req.body);
+  await product.save();
+
+  res.redirect(`${systemConfig.prefixAdmin}/products`);
 };
