04176e7d900b342ee0e4b07ddbf4258708d8b568
QuocHuyGIT103 <quochuyab2003@gmail.com>
2025-02-22 21:34:25 +0700

    tính năng tạo sp với upload ảnh

--- product-management/routes/admin/product.route.js
+++ product-management/routes/admin/product.route.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const storageMulter = require("../../helpers/storageMulter");
+const multer = require("multer");
+const upload = multer({ storage: storageMulter() });
+
 const controller = require("../../controllers/admin/product.controller");
 
 router.get("/", controller.index);
@@ -12,6 +16,7 @@ router.patch("/change-multi", controller.changeMulti);
 router.delete("/delete/:id", controller.deleteItem);
 
 router.get("/create", controller.create);
-router.post("/create", controller.createPost);
+
+router.post("/create", upload.single("thumbnail"), controller.createPost);
 
 module.exports = router;
