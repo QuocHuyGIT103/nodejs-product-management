8ba46e9e452be214f1793296ac23015942306b02
QuocHuyGIT103 <quochuyab2003@gmail.com>
2025-02-26 18:02:30 +0700

    done detail product for client

--- product-management/controllers/client/product.controller.js
+++ product-management/controllers/client/product.controller.js
@@ -13,9 +13,33 @@ module.exports.index = async (req, res) => {
     return product;
   });
 
-  console.log(newProducts);
+  // console.log(newProducts);
   res.render("client/pages/products/index", {
     pageTitle: "Products Page",
     products: newProducts || [],
   });
+};
+
+// [GET] /products/:slug
+module.exports.deltail = async (req, res) => {
+  console.log(req.params.slug);
+  try {
+    const find = {
+      deleted: false,
+      slug: req.params.slug,
+      status: "active",
+    };
+
+    const product = await Product.findOne(find);
+
+    console.log(product);
+
+    res.render("client/pages/products/detail", {
+      pageTitle: product.title,
+      product: product || {},
+      message: { success: req.flash("success")[0] || "" },
+    });
+  } catch (error) {
+    res.redirect(`/products`);
+  }
 };
--- product-management/routes/client/product.route.js
+++ product-management/routes/client/product.route.js
@@ -1,9 +1,9 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
 
 const controller = require("../../controllers/client/product.controller");
 
-
-router.get('/', controller.index);
+router.get("/", controller.index);
+router.get("/:slug", controller.deltail);
 
 module.exports = router;
