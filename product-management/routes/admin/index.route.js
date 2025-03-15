379b55c939e6fddab3ab881692c84a6950ed235b
QuocHuyGIT103 <quochuyab2003@gmail.com>
2025-03-15 23:09:04 +0700

    feature category product

--- product-management/routes/admin/index.route.js
+++ product-management/routes/admin/index.route.js
@@ -1,12 +1,15 @@
-const dashboardRoute = require('./dashboard.route');
-const systemConfig = require('../../config/system')
+const dashboardRoute = require("./dashboard.route");
+const systemConfig = require("../../config/system");
 
-
-const productRoute = require('./product.route');
+const productRoute = require("./product.route");
+const productCategoryRoute = require("./product-category.route");
 
 module.exports = (app) => {
-    const PATH_ADMIN = systemConfig.prefixAdmin;
-    app.use(PATH_ADMIN + '/dashboard', dashboardRoute);
+  const PATH_ADMIN = systemConfig.prefixAdmin;
 
-    app.use(PATH_ADMIN + '/products', productRoute);
-}
+  app.use(PATH_ADMIN + "/dashboard", dashboardRoute);
+
+  app.use(PATH_ADMIN + "/products", productRoute);
+
+  app.use(PATH_ADMIN + "/products-category", productCategoryRoute);
+};
